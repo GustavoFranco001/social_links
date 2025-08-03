6295b6263da4d9574ba679690dfca4afb8138913
Gustavo Franco <gfcivil0@gmai.com>
2025-08-02 21:17:54 -0400

    uptade background

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-function toggleMode() {
-  const html = document.documentElement
+// function toggleMode() {
+//   const html = document.documentElement
 
-  // if (html.classList.contains("light"))
-  //   html.classList.remove("light")
-  // else
-  //   html.classList.add("light")
+//   // if (html.classList.contains("light"))
+//   //   html.classList.remove("light")
+//   // else
+//   //   html.classList.add("light")
 
-  html.classList.toggle("light")
+//   html.classList.toggle("light")
 
-  // pegar a tag img
-  const img = document.querySelector("#profile img")
-  // substituir a img
-  if (html.classList.contains("light")) {
-    // se tiver em light mode, usar img light mode
-    img.setAttribute("src", "./assets/habit/hbt-light.png")
-    // se tiver em light mode, usar img light mode
-  } else {
-    img.setAttribute("src", "./assets/habit/hbt-dark.png")
-  }
-}
+//   // pegar a tag img
+//   const img = document.querySelector("#profile img")
+//   // substituir a img
+//   if (html.classList.contains("light")) {
+//     // se tiver em light mode, usar img light mode
+//     img.setAttribute("src", "./assets/habit/hbt-light.png")
+//     // se tiver em light mode, usar img light mode
+//   } else {
+//     img.setAttribute("src", "./assets/habit/hbt-dark.png")
+//   }
+// }
